539d68d88fb6c2992f9048159aa2ecb8f7965d0b
ike barranco <barranco.ike@outlook.com>
2020-05-25 00:12:33 -0600

    Added initial file structure and completed schema/seed sql

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name)
+VALUES ("BIG MAC"),
+("BIG CARL"),
+("IN-N-OUT BURGER");
+
+SELECT * FROM burgers;
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mysql = require("mysql");
+
+const server = express();
+
+const PORT = process.env.PORT || 8080;
+
+// Parse request body as JSON
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
+server.engine("handlebars", exphbs({ defaultLayout: "main" }));
+server.set("view engine", "handlebars");
+
+server.listen(PORT, ()=>{
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+})
