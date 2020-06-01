45ef4886b31bd1df9ef368063cafb6973bb74110
ike barranco <barranco.ike@outlook.com>
2020-06-01 05:42:50 -0600

    Added comments to the sql dabase files

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,17 @@
+-- Used for initial creation of DB
 DROP DATABASE IF EXISTS burgers_db;
-
+-- Create Database
 CREATE DATABASE burgers_db;
-
+-- Use burgers_db
 USE burgers_db;
 
+-- burgers
+--      id: placement of item
+--      burger_name: name of the burger
+--      devoured: boolean to claim if the burger has been eaten or not
 CREATE TABLE burgers(
     id INTEGER NOT NULL AUTO_INCREMENT,
     burger_name VARCHAR(255),
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY(id)
-);
+);--End burgers Table
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,8 @@
+-- Use burgers_db
 USE burgers_db;
 
+-- Insert test values into the burgers table
 INSERT INTO burgers(burger_name)
 VALUES ("BIG MAC"),
 ("BIG CARL"),
 ("IN-N-OUT BURGER");
-
--- UPDATE burgers SET devoured = true WHERE id = 3;
-
--- SELECT * FROM burgers;
