2809e58451d01b50ef23edb0fbc66ccddea4afa2
ike barranco <barranco.ike@outlook.com>
2020-06-01 06:07:53 -0600

    Added comments to public js file

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,21 +1,36 @@
+/**
+ * .devourBtn click Event
+ * Purpose: To update burger information when clicked
+ *          Then reload page
+ */
 $(".devourBtn").on("click", function(event){
+    // grab event id from button
     const $pressedButton = $(event.target);
     const ateBurger = {devoured: 1};
+    // Ajax call to server to update information
     $.ajax("/api/burgers/" + $pressedButton.data("id"),{
         method: "PUT",
         data: ateBurger
     }).then(function(){
+        //Reload the page
         location.reload();
-    });
-    
-});
+    });//End ajax call
+});//End .devourBtn click event
 
+/**
+ * .deleteBtn click Event
+ * Purpose: To delete burger information when clicked
+ *          Then reload page
+ */
 $(".deleteBtn").on("click", function(event){
+    //Grab clickevent
     const $pressedButton = $(event.target);
+    //ajax call to delete burger information
     $.ajax("/api/burgers/" + $pressedButton.data("id"),{
         method: "DELETE",
     }).then(function(){
+        //Reload the page
         location.reload();
-    });
-});
+    });//End ajax delete call
+});//End .deleteBtn click Event
 
